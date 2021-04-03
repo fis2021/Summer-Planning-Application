cadddef10b55e957afc49dba3fd579441f2aa401
Ulkei Szabolcs <szabolcs.ulkei@student.upt.ro>
2021-04-03 14:39:09 +0300

    Setup

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main{
+    public static void main(String[] args) {
+        Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+
+        LOGGER.info("this is an info log");
+    }
+}
