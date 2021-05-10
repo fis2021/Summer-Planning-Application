73f6796a881eb8876803738c3effefbd7e576eec
Sturzu Mihai <sturzumihaita@gmail.com>
2021-05-10 22:25:30 +0300

    <sm-32> created user class(model)

---
+++ src/main/java/Model/User.java
@@ -0,0 +1,40 @@
+package Model;
+
+public class User {
+    private String userID;
+    private String username;
+    private String role;
+    private String password;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+}
